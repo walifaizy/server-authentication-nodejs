9122e830c8e79608329380141c2b29e7c4699a49
wal faizy <faizy.wali@gmail.com>
2017-04-22 18:37:42 +0400

    pre final commit

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,22 @@ const User = require("../models/user");
 const config = require("../config");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const localStrategy = require("passport-local");
+
+// Create local strategy
+const localLogin = new localStrategy({usernameField: "email"}, function(email, password, done) {
+    User.findOne({email: email}, function(err, user) {
+        if(err) {return done(err);}
+        if(!user) {return done(null, false);}
+        
+        User.comparePassword(password, function(err, isMatch) {
+            if(err) {return done(err);}
+            if(!isMatch) {return done(null, false);}
+            
+            return done(null, user);
+        });
+    });
+});
 
 // Setup  options for jwt strategy
 const jwtOptions = {
@@ -26,3 +42,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 // Tell passport to use jwt login
 
 passport.use(jwtLogin);
+passport.use(localLogin);
